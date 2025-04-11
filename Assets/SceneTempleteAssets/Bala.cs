0ae1376b8efb0ec644aaa76e0a1234c85294bacb
Braian Arguello <braian.arguello@hotmail.com>
2025-04-10 23:13:33 -0300

    [ADD se agrega prefab Bala]

---
+++ Assets/SceneTempleteAssets/Bala.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bala : MonoBehaviour
+{
+    //Caracteristicas importantes para considerar de una bala es que tiene que tener una velocidad y un tiempo de vida. 
+    public float velocidadBala = 10f;
+    public float vidaBala = 5f;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
